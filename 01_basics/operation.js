cc885c0b9f96207150a41b78204d32034212e982
uttkarshnjr10 <133018446+uttkarshnjr10@users.noreply.github.com>
2025-03-26 17:53:16 +0000

    operations and pre and post increment

---
+++ 01_basics/operation.js
@@ -0,0 +1,18 @@
+let value = 33
+let negative_value = -value
+console.log(negative_value)
+
+let num = 100
+num++;
+console.log(num)
+//in the pre increment , first the value will be updated and then it will assign to variable
+// in the post increment , first the value will be assign and then it will be increment
+
+
+let num1, num2 , num3 
+
+num1 = num2 = num3 = 2+2;
+
+// conclusion
+// the code should be in the readable form and easy to understand to the other people , 
+// so we should assign any value to variables clearly
