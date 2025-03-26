8e71257f44dbd40807ca5b0e4762318688dc859a
uttkarshnjr10 <133018446+uttkarshnjr10@users.noreply.github.com>
2025-03-26 16:50:53 +0000

    data type conversion

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,28 @@
+let score = 20
+
+console.log(typeof score);
+
+let MatchScore = Number(score)
+console.log( typeof MatchScore)
+console.log(MatchScore)
+
+//33 = "33"
+// 33abc = Nan
+
+let result = 0
+let FinalResult = Boolean(result)
+console.log(typeof FinalResult)
+console.log(FinalResult)
+
+// 0 = false and 1 = true on boolean operations
+let age = 35
+let data = String(age)
+console.log(typeof data)
+console.log(data)
+
+//conclusion
+
+//empty string ""= false
+//"abc" = true 
+
+
