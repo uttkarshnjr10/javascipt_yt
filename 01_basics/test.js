822038ae059e547c599dbf41971813a9d7eb2336
uttkarshnjr10 <133018446+uttkarshnjr10@users.noreply.github.com>
2025-03-26 13:24:21 +0000

    datatypes

---
+++ 01_basics/test.js
@@ -0,0 +1,13 @@
+console.log("utkarsh")
+let name = "aman"
+console.log(name)
+// number
+// null - standalone value 
+let address;
+//this is undefined datatype
+//boolean - true or false
+//big int 
+//symbol - unique
+//string = ""
+address = "patna";
+console.log(address)
